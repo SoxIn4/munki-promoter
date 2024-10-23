abf37ad20aceef6ee8a72f129051dfac4121b6ea
SoxIn4 <soxin4@comcast.net>
2024-10-23 14:06:50 -0500

    more list fixes

--- munki-promoter.py
+++ munki-promoter.py
@@ -340,23 +340,27 @@ def prep_all_promotions(config, munki_path, config_path):
 						# prep individual pkginfo for promotion
 						for promotion in config["promotions"]:
 							promotion_targets = config["promotions"][promotion].get('targeted_names', None)
-							promotion_exclusions = config["promotions"][promotion].get('excluded_names', None)		
+							promotion_exclusions = config["promotions"][promotion].get('excluded_names', None)
+
 							if global_excluded:
 								exclusions = set(global_excluded)
 								if promotion_exclusions:
 									exclusions = exclusions + promotion_exclusions
 							else:
 								exclusions = None if not promotion_exclusions else set(promotion_exclusions)
+
 							if global_targets:
 								targets = set(global_targets)
 								if promotion_targets:
 									targets = targets + promotion_targets
 								if exclusions:
 									targets = targets - exclusions
-							else:
+							elif promotion_targets:
 								targets = set(promotion_targets)
 								if exclusions:
 									targets = targets - exclusions
+							else:
+								targets = None
 							promote_to, promote_from, days, custom_items = get_promotion_info(promotion, promotions, config, config_path)
 							item_name, item_version, item_promotion, custom_promote_to = prep_item_for_promotion(pkginfo, promote_to, promote_from, days, custom_items, file, exclusions, targets)
 							if item_name: # would be None if not eligible for promotion
